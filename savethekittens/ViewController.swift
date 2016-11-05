f775e9fbd7b0776f53355bc787dd877cdb125a41
Choo Wei Kian <weikian92@hotmail.com>
2016-11-05 14:00:29 +0800

    Initial Commit

---
+++ savethekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekittens
+//
+//  Created by Choo Wei Kian on 05/11/2016.
+//  Copyright © 2016 Republic Polytechnic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
